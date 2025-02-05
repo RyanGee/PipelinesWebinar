4e2b26752c62a488bdf89a303148a428096a0cd8
Ryan Gee <Ryan.Gee@red-gate.com>
2025-02-05 11:17:04 +0000

    Alter customers add column2

--- schema-model/Tables/dbo.contacts.sql
+++ schema-model/Tables/dbo.contacts.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[contacts]
 [email] [varchar] (255) NULL,
 [phone] [varchar] (255) NULL,
 [address] [varchar] (255) NULL,
-[column1] [varchar] (255) NULL
+[column1] [varchar] (255) NULL,
+[column2] [varchar] (255) NULL
 )
 GO
