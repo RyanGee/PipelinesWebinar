292c2eff4256d571e3397204f8c615900040df05
Ryan Gee <Ryan.Gee@red-gate.com>
2025-02-05 10:43:22 +0000

    alter contacts add address

--- schema-model/Tables/dbo.contacts.sql
+++ schema-model/Tables/dbo.contacts.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[contacts]
 (
 [name] [varchar] (255) NULL,
 [email] [varchar] (255) NULL,
-[phone] [varchar] (255) NULL
+[phone] [varchar] (255) NULL,
+[address] [varchar] (255) NULL
 )
 GO
